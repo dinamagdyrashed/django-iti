6e25fca6ef32e3eb89762931f500cf4cb26540d3
dinamagdyrashed <dinarashid78@gmail.com>
2025-08-26 11:06:29 +0300

    Day Three

--- ourproject/track/models.py
+++ ourproject/track/models.py
@@ -4,3 +4,11 @@ from django.db import models
 class Track(models.Model):
     id=models.AutoField(primary_key=True)
     name=models.CharField(max_length=50,null=False)
+
+    @classmethod
+    def getalltracks(cls):
+        return cls.objects.all()
+
+    @classmethod
+    def gettrackbyid(cls,id):
+        return cls.objects.get(id=id)
--- ourproject/trainee/views.py
+++ ourproject/trainee/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,HttpResponseRedirect
 from .models import Trainee
+from track.models import Track
 # Create your views here.
 def alltrainee(request):
     context={}
-    context['trainees']=Trainee.objects.all()
+    context['trainees']=Trainee.getalltrainees().order_by('id')
     # trainees=[[1,'John Doe'],[2,'Jane Smith'],[3,'Mike Johnson'],[4,'Emily Davis']]
     return render(request,'trainee/list.html',context)
 
@@ -12,16 +13,32 @@ def gettraineeid(request):
     return HttpResponse('<h1>Hello from id trainee</h1>')
 
 def updatetrainee(request,id):
-    return HttpResponse(f'<h1>Hello from update trainee {id}</h1>')
+    trainee=Trainee.gettraineebyid(id)
+    context={'trainee':trainee}
+    context['tracks']=Track.getalltracks()
+    if request.method=='POST':
+        trainee.name=request.POST['trname']
+        trainee.email=request.POST['tremail']
+        trainee.trackid=Track.gettrackbyid(request.POST['trtrack'])
+        if request.FILES.get('trimage'):
+            trainee.image=request.FILES['trimage']
+        trainee.save()
+        # return HttpResponseRedirect('/Trainee/')
+        return redirect('alltrainee')
+    return render(request,'trainee/update.html',context)
 
 def inserttrainee(request):
     print(request.POST)
+    context={}
+    context['tracks']=Track.getalltracks()
     if request.method=='POST':
-        name=request.POST['trname']
-        email=request.POST['tremail']
-        image=request.FILES['trimage']
-        Trainee.objects.create(name=name,email=email,image=image)
-    return render(request,'trainee/insert.html')
+        # trackid=request.POST['trtrack']
+        # trackobj=Track.objects.get(id=trackid)
+        Trainee.objects.create(name=request.POST['trname'],email=request.POST['tremail'],image=request.FILES['trimage'],trackid=Track.gettrackbyid(request.POST['trtrack']))
+        return redirect('alltrainee')
+    return render(request,'trainee/insert.html',context)
 
 def deletetrainee(request,id):
-    return HttpResponse(f'<h1>trainee {id} deleted</h1>')
+    # Trainee.objects.filter(id=id).delete()
+    Trainee.objects.filter(id=id).update(status=False)
+    return redirect('alltrainee')
